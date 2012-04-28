4530bf59aadd70e427d2fbba1eb71843a1f7b641
Brett Porter <brett@apache.org>
2012-04-28 21:16:38 +1000

    [JRUBY-6235] make sure the 'in' field is located on the base class, as it would always fail locating on BufferedInputStream. This allows the process output to be read as a FileChannel instead of a BufferedInputStream, as it is with input.

--- src/org/jruby/util/ShellLauncher.java
+++ src/org/jruby/util/ShellLauncher.java
@@ -35,6 +35,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilterInputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -757,7 +758,7 @@ public class ShellLauncher {
         while (filteredStream instanceof BufferedInputStream) {
             try {
                 filteredStream = (InputStream)
-                    FieldAccess.getProtectedFieldValue(BufferedInputStream.class,
+                    FieldAccess.getProtectedFieldValue(FilterInputStream.class,
                         "in", filteredStream);
             } catch (Exception e) {
                 break; // break out if we've dug as deep as we can
